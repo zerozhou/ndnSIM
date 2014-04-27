6f26f73005e5f8f6632c5cd698a09a049db833b2
zijian <zjzhou@sharePC.(none)>
2014-04-27 22:02:25 +0800

    ndnsim - 0427

---
+++ scenarios/disabled/uint32_t_value_test.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+
+#include <boost/multi_index/member.hpp>
+#include <boost/ref.hpp>
+int main()
+{
+    uint32_t seq = std::numeric_limits<uint32_t>::max();
+    std::cout << seq<<"\n";
+    return 0;
+}
